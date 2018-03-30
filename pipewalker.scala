0b257ba175413786757833d9359f5a99ee36cf2a
Lassi Heikkinen <lassi.heikkinen@aalto.fi>
2018-03-30 04:37:11 +0300

    seems to work

--- pipewalker.scala
+++ pipewalker.scala
@@ -11,13 +11,12 @@ import java.awt.Point
  * Only the space character (' ', ASCII 0x20) will be used to denote empty or inaccessible tiles.
  * 
  * The graph is directed and one-way connections are possible. Consider the following input:
- * 		"#-|"
+ * 		"#-|\n" +
  * 		"  #"
  * The longest path is 4 steps from the end piece in the top left through the horizontal connector to the vertical connector and bottom right end piece.
  * There is no valid path starting from the bottom right end piece.
  */
 class PipeWalker {
-  // TODO: Add some private methods and modify longestPipe to solve the problem
   def longestPipe(inputText: String): Int = {
     val (grid, startPositions) = init(inputText)  
     return dfs(grid, startPositions/*.par*/)
@@ -25,26 +24,36 @@ class PipeWalker {
 
  
   case class Node(piece: Char, neighbours: List[Point]) {
-    def toString() = piece.toString()
-    def mkString() = toString()
+    override def toString() = piece.toString()
+    //def mkString() = toString()
   }
   
   
   private def dfs(grid: Array[Array[Node]], startPositions: Buffer[Point]):Int = {
     @volatile var longestPath = 0
     
-    startPositions.foreach(p => inner(grid, Set(p), grid(p.y)(p.x), 1))
+    //startPositions.foreach(p => inner(grid, Set(p), grid(p.y)(p.x), 1))
+    for (p <- startPositions) {
+      //println("starting search at : " +p.y + "," + p.x)
+      inner(grid, Set(p), grid(p.y)(p.x), 1)
+      //println("\n\n\n")
+    }
     
     def inner(grid: Array[Array[Node]], visited: Set[Point], currentNode: Node, depth: Int): Unit = {
-      
+      //println("depth: " + depth)
       //println(currentNode.piece)
       if (currentNode.piece == '#') {
+        //println("found endpiece")
         longestPath = max(longestPath, depth)
       }
-      
+      //println("number of neighbours: " + currentNode.neighbours.length.toString())
+      //println("neighbours are: " + currentNode.neighbours.mkString(" "))
+      //println("visited nodes are: " + visited.mkString(""))
       for (neighbour <- currentNode.neighbours) {
         if (!visited.contains(neighbour)) {
+          //println("recursing to: " + neighbour.y.toString() + "," + neighbour.x.toString())
           inner(grid, visited + neighbour, grid(neighbour.y)(neighbour.x), depth+1)
+          //println("return from recursion at depth " + depth)
         }
       }
     }
@@ -84,7 +93,7 @@ class PipeWalker {
       if (x >= 0 && x+1 < arr(0).length && arr(y)(x+1) != ' ') {ret.append(new Point(x+1, y))}
       return ret.toList
     }
-    def getVerticalNeighbours(x: Int, y: Int):List[Point] = {
+    def getVerticalNeighbours(y: Int, x: Int):List[Point] = {
       val ret = Buffer[Point]()
       if (y-1 >= 0 && y < arr.length && arr(y-1)(x) != ' ') {ret.append(new Point(x, y-1))}
       if (y >= 0 && y+1 < arr.length && arr(y+1)(x) != ' ') {ret.append(new Point(x, y+1))}
@@ -101,6 +110,9 @@ case class TestCase(testInput: String, expectedLength: Int)
 object TestCases {
   // TODO: You probably want to add more test cases here
 
+  private val level0 =
+    "#"
+  
   private val level1 =
     "  +--+   \n" +
     "#-+  |   \n" +
@@ -117,14 +129,38 @@ object TestCases {
     " #-# +---# "
 
   private val level4 =
-    "#++#"
+    "#++#\n" + 
+    " ++ "
+  
+  private val level5 = 
+    "#+#\n" + 
+    "|-|\n" + 
+    "#+#"
+    
+  private val level6 =
+    "#-|\n" + 
+    "  #"
+  private val level999 = //perf test
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n" + 
+    "##########\n"
     
   val tests = List(
     TestCase("", 0),
-    //TestCase(level1, 9),
-    //TestCase(level2, 6),
-    //TestCase(level3, 11),
-    TestCase(level4, 3)
+    TestCase(level0, 1),
+    TestCase(level1, 9),
+    TestCase(level2, 6),
+    TestCase(level3, 11),
+    TestCase(level4, 6),
+    TestCase(level5, 9),
+    //TestCase(level999, 100)
   )
 }
 
@@ -138,6 +174,9 @@ class PipeWalkerTest(walker: PipeWalker) {
   def printResults(results: List[TestResult]): Unit =
     results.map(formatResult).foreach(println)
 
+  def printFailsOnly(results: List[TestResult]) =
+    results.filter(_.didPass).map(formatResult).foreach(println)
+    
   private def formatResult(result: TestResult): String = {
     val test = result.testCase
     val passText = if (result.didPass) "PASSED" else "FAILED"
